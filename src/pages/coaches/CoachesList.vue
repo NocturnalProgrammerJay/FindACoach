2598e831c7f7f8a701018a2566f49696742863f5
Jamar Andrade <jandrade2018@fau.edu>
2023-09-05 13:06:36 -0400

    wired up pages, made sure they all worked/connected

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -2,7 +2,15 @@
     <section>
         FILTER
     </section>
+    <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register"></router-link>
+    </div>
     <section>
         LIST OF COACHES
     </section>
 </template>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import CoachesList from './pages/coaches/CoachesList.vue'
 import CoachRegistration from './pages/coaches/CoachRegistration.vue'
 import ContactCoach from './pages/requests/ContactCoach.vue'
 import RequestsReceived from './pages/requests/RequestsReceived.vue'
-import NotFound from './pages/requests/NotFound.vue'
+import NotFound from './pages/NotFound.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -16,7 +16,7 @@ const router = createRouter({
             path: '/coaches/:id', 
             component: CoachDetail, 
             children: [
-            {path: '/contact', component: ContactCoach}]
+            {path: 'contact', component: ContactCoach}]
         },
         {path: '/register', component: CoachRegistration},
         {path: '/request', component: RequestsReceived},
