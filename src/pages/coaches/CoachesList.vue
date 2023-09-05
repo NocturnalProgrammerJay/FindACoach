f58db93b31b11825a172894550a527eb96c30510
Jamar Andrade <jandrade2018@fau.edu>
2023-09-05 13:52:42 -0400

    created coachItem and past in props from module coaches/getters array

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -4,24 +4,36 @@
     </section>
     <div class="controls">
         <button>Refresh</button>
-        <router-link to="/register"></router-link>
+        <router-link to="/register">Register as Cpach</router-link>
     </div>
     <ul v-if="hasCoaches">
-        <li v-for="coach in filteredCoaches" :key="coach.id">
-            {{ coach.firstName }}
-        </li>
+        <coach-item 
+            v-for="coach in filteredCoaches" 
+            :key="coach.id" 
+            :id="coach.id"
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+        ></coach-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
 </template>
 
 <script>
+    import CoachItem from '../../components/coaches/CoachItem.vue'
+
     export default {
+        components: {
+            CoachItem
+        },
         computed: {
             filteredCoaches(){
+                console.log(this.$store.getters['coaches/coaches']);
                 return this.$store.getters['coaches/coaches']
             },
             hasCoaches(){
-                return this.$store.getters['coaches/hasCoaches ']
+                return this.$store.getters['coaches/hasCoaches']
             }
         },
     }
