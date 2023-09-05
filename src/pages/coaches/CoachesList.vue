e9df98f863a93e4f73a695ce4585ab1f641c40c9
Jamar Andrade <jandrade2018@fau.edu>
2023-09-05 13:29:33 -0400

    set up store and modules for getters, actions, and mutations

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,11 +6,23 @@
         <button>Refresh</button>
         <router-link to="/register"></router-link>
     </div>
-    <section>
-        LIST OF COACHES
-    </section>
+    <ul v-if="hasCoaches">
+        <li v-for="coach in filteredCoaches" :key="coach.id">
+            {{ coach.firstName }}
+        </li>
+    </ul>
+    <h3 v-else>No coaches found.</h3>
 </template>
 
-<style>
-
-</style>
+<script>
+    export default {
+        computed: {
+            filteredCoaches(){
+                return this.$store.getters['coaches/coaches']
+            },
+            hasCoaches(){
+                return this.$store.getters['coaches/hasCoaches ']
+            }
+        },
+    }
+</script>
